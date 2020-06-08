1f396d3a2b2370904d0da77e4c774da4734832aa
isaachouston <isaacprh@gmail.com>
2020-06-08 18:02:04 -0300

    Updates

---
+++ precache-manifest.f79e85b713405967380971e001976648.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b2391132c8adf9b8c539cd1267e6f3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb27934a2c4334a983f8",
+    "url": "/static/css/main.3f66b888.chunk.css"
+  },
+  {
+    "revision": "8c8125dedfa68e0b0fea",
+    "url": "/static/js/2.c385289a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c385289a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb27934a2c4334a983f8",
+    "url": "/static/js/main.426b4aae.chunk.js"
+  },
+  {
+    "revision": "1e967e60c8804a474105",
+    "url": "/static/js/runtime-main.9050acbe.js"
+  },
+  {
+    "revision": "e07968da6e529509508e873fc5eb9826",
+    "url": "/static/media/OrtisanSignature.e07968da.ttf"
+  },
+  {
+    "revision": "0f7dc600f19ac05e8c5e9e845d600428",
+    "url": "/static/media/coding.0f7dc600.svg"
+  },
+  {
+    "revision": "566d99eb20b830d3170cc4cb4c183e78",
+    "url": "/static/media/developer_activity.566d99eb.svg"
+  },
+  {
+    "revision": "4b6fdfa7973397c591e6ea5fb8e5853d",
+    "url": "/static/media/message.4b6fdfa7.svg"
+  }
+]);
